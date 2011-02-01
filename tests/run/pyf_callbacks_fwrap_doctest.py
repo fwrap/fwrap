c4f44386ab6cca64752406c096cdb544b9db2c00
Dag Sverre Seljebotn <dagseljebotn@gmail.com>
2011-02-01 14:40:12 +0100

    Forgot to commit this long ago...

---
+++ tests/run/pyf_callbacks_fwrap_doctest.py
@@ -0,0 +1,26 @@
+from pyf_callbacks_fwrap import *
+import numpy as np
+
+def twice(x, extra=None):
+    if extra is not None:
+        assert np.all(x == extra)
+    return 2 * x
+
+def twice_with_extra(x, a, b, c):
+    assert (a, b, c) == (1, 2, 3)
+    return 2 * x
+
+__doc__ = u'''
+
+    >>> foo(twice, 3)
+    6
+
+#    >>> foo(twice_with_extra, 3, callback_extra_args=(1, 2, 3))
+#    6
+
+    >>> array2d(twice, np.ones((3, 3)), 3, 3)
+    array([[ 2.,  2.,  2.],
+           [ 2.,  2.,  2.],
+           [ 2.,  2.,  2.]])
+
+'''
