f54219d4c2095ca3afd84f2868a561444822d5c8
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-03-23 13:32:57 +0100

    Refactor of inplace tool

--- fwrap/resources/tools/inplace.py
+++ fwrap/resources/tools/inplace.py
@@ -22,33 +22,42 @@ import os
 from waflib.Configure import conf
 from waflib.TaskGen import after_method, before_method, feature, taskgen_method, extension
 
-# Note: Python extensions will have their install_path set twice,
-# but we use after_method to make sure the pyext setting prevails
+def _find_extension_dir_node(sources):
+    srcpath = None
+    for x in sources:
+        if x.is_src() and (x.name.endswith('.pyx') or x.name.endswith('.c')):
+            srcpath = x.parent
+    return srcpath
 
-@feature('cshlib', 'fcshlib')
-@before_method('propagate_uselib_vars', 'apply_link')
-def apply_install_path_shlib(self):
-    if self.env['INPLACE_INSTALL']:
-        self.install_path = self.bld.srcnode.make_node('lib').abspath()
-        self.rpath = '${ORIGIN}'
+@feature('cshlib', 'fcshlib', 'pyext')
+@before_method('propagate_uselib_vars', 'apply_link', 'init_pyext')
+def apply_inplace_install_path(self):
+    if self.env['INPLACE_INSTALL'] and not getattr(self, 'install_path', None):
+        if 'pyext' in self.features:
+            # Scan sources for likely position of extension source
+            srcpath = _find_extension_dir_node(self.source)
+            if srcpath is None:
+                print self.source
+                print AssertionError("Python extension does not have an associated C file...")
+                return
+            self.install_path = os.path.join(self.bld.srcnode.abspath(), srcpath.srcpath())
+        else:
+            self.install_path = self.bld.srcnode.make_node('lib').abspath()
 
-@feature('pyext')
-@before_method('propagate_uselib_vars', 'apply_link')
-@after_method('apply_install_path_shlib')
-def apply_install_path_pyext(self):
-    if self.env['INPLACE_INSTALL']:
-        # Scan sources for likely position of extension source
-        srcpath = None
-        for x in self.source:
-            if x.is_src() and (x.name.endswith('.pyx') or x.name.endswith('.c')):
-                srcpath = x.parent
-        if srcpath is None:
-            raise AssertionError("Python extension does not have an associated C file...")
-        self.install_path = os.path.join(self.bld.srcnode.abspath(), srcpath.srcpath())
-
-        # Set rpath
-        lib_path  = self.bld.srcnode.make_node('lib')
-        self.rpath = os.path.join('${ORIGIN}', lib_path.path_from(srcpath))
+@feature('cshlib', 'fcshlib', 'pyext')
+@before_method('propagate_uselib_vars', 'apply_link', 'init_pyext')
+def apply_inplace_rpath(self):
+    if self.env['INPLACE_INSTALL'] and not getattr(self, 'rpath', None):
+        if 'pyext' in self.features:
+            srcpath = _find_extension_dir_node(self.source)
+            if srcpath is None:
+                print self.source
+                print AssertionError("Python extension does not have an associated C file...")
+                return
+            lib_path  = self.bld.srcnode.make_node('lib')
+            self.rpath = os.path.join('${ORIGIN}', lib_path.path_from(srcpath))
+        else:
+            self.rpath = '${ORIGIN}'
 
 def options(self):
     self.add_option('--inplace', action='store_true',
